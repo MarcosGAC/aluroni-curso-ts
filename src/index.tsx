2109434992e7ee2b8442eb0138759469841a9cac
MarcosGAC <marcosgabriel.dev@gmail.com>
2023-04-04 18:29:29 -0300

    add imgs

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Cardapio from "./pages/Cardapio";
-
+import Router from "./routes";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-	<React.StrictMode>
-		<Cardapio />
-	</React.StrictMode>
+  <React.StrictMode>
+    <Router />
+  </React.StrictMode>
 );
-
-
---
+++ src/routes.js
@@ -0,0 +1,23 @@
+import Cardapio from "pages/Cardapio";
+import Inicio from "pages/Inicio";
+import Sobre from "pages/Sobre";
+import Menu from "pages/components/Menu";
+import PaginaPadrao from "pages/components/PaginaPadrao";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+export default function AppRouter() {
+  return (
+    <main>
+      <Router>
+        <Menu />
+        <Routes>
+          <Route path="/" element={<PaginaPadrao />}>
+            <Route index element={<Inicio />} />
+            <Route path="cardapio" element={<Cardapio />} />
+            <Route path='sobre' element={<Sobre />} /> 
+          </Route>
+        </Routes>
+      </Router>
+    </main>
+  );
+}
